4a7dde85ac47dae4632f622070202adc980e8730
George <george@example.com>
2026-02-14 13:42:13 +0930

    Add .gitignore to separate research usage from SDK

--- run_enhanced_bridge.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/grge/code/trove/packages/trove-sdk
-exec uv run python ../../openclaw_mcp_bridge_v2.py "$@"
--- run_research_curator.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/grge/code/trove
-TROVE_API_KEY=zkx4Pf9cnEaOQ6MRW0P2MR7WAFV5iTeu python research_curator_agent.py "$@"
